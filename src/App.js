4bfa6884dbe44915ff46377454d61e23d533cd63
Nicolas <nicolas.kittsteiner@telefonica.com>
2017-09-29 21:29:18 -0300

    refactor: Adding SearchBook component - Changing html code and moving to the new component - Handling state change for closing the component and return to initial state - Small fix in color for titles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
+import SearchBook from './SearchBook'
+import * as BooksAPI from './BooksAPI'
 
 class BooksApp extends React.Component {
   state = {
@@ -13,30 +15,24 @@ class BooksApp extends React.Component {
     showSearchPage: false
   }
 
+  componentDidMount = () => {
+    BooksAPI.getAll().then((books) => {
+      console.log('books', books)
+    })
+  }
+
+  closeSearch = () => {
+    this.setState((state) =>({
+      showSearchPage: false
+    }))
+  }
+
   render() {
     return (
       <div className="app">
         {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
+          <SearchBook 
+            onCloseSearch={this.closeSearch} />
         ) : (
           <div className="list-books">
             <div className="list-books-title">
